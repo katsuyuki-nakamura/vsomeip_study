f143a14d808d657e1f0e3375296c111989c1227a
Katsuyuki Nakamura <Katsuyuki.Nakamura@gmail.com>
2023-10-12 17:35:49 +0900

    1st commit

---
+++ vsomeip_service_example/build/CMakeFiles/service_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/service_example.dir/src/vsomeip_service_example.cpp.o"
+  "CMakeFiles/service_example.dir/src/vsomeip_service_example.cpp.o.d"
+  "service_example"
+  "service_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/service_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
